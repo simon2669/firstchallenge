3995b47565beaf998f83a03b5417c77e2cdea2a9
simon2669 <feltalalo26@gmail.com>
2021-02-22 17:29:17 +0100

    végleges megoldás

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# firstchallenge
+# firstchallenge.
+
+Hello Boris!
+
+ I hope you won't search for the cheapest beer when I will be at your pub!
+
+Simon
--- app.js
+++ app.js
@@ -1,161 +1,177 @@
 'use strict';
+import { beerList } from "./beers.js";
 
-import {beerList} from "./beers.js";
 
-function elsoFeladat(){
-
-    let solution=[];
-    let beers=[];
+// !Group beers by brand
+function elsoFeladat() {
+    let solution = [];
+    let beers = [];
     let köztes = {};
-
     let brands = new Set();
 
-
     //sör brandek kiválogatása
-    beerList.forEach((sörök)=>{
+    beerList.forEach((sörök) => {
         brands.add(sörök.brand)
-    })
-    brands=Array.from(brands);
+    });
+    brands = Array.from(brands);
 
-
-    for (let index = 0; index <brands.length; index++) {
-        beers=[];
+    for (let index = 0; index < brands.length; index++) {
+        beers = [];
         // sörnevek (ismétlés nélküli) kiválogatása brand szerint
-        beerList.forEach((item)=>{
-            if (brands[index]==item.brand) {
+        beerList.forEach((item) => {
+            if (brands[index] == item.brand) {
                 beers.push(item)
                 // if (!beers.includes(item.name)) {                   
                 // }
             }
-        })
+        });
+
         //hozzáadás a megoldás array-hez
-        köztes={
+        köztes = {
             'brand': brands[index],
             'beers': beers
         };
         solution.push(köztes);
-        //console.log(beers) 
-    }  
-    //console.log(solution)
+    }
     return solution;
 };
-(function masodikFeladat(beerType='Szűrt'){
+
+// !Filter brands by BeerType
+function masodikFeladat(beerType) {
     let megoldas = [];
-    beerList.forEach((item)=>{
-        if (item.type==beerType) {
+    beerList.forEach((item) => {
+        if (item.type == beerType) {
             megoldas.push(item);
         }
-    })
-    //console.log(megoldas)
+    });
+    if (megoldas.length == 0) {
+        alert('Kérlek adj meg egy valós típust "" jelek között!');
+        return false;
+    }
     return megoldas;
-})();
+};
 
-
-(function harmadikFeladat(){
-    let megoldas=[];
-    let a =elsoFeladat();
+// Get the cheapest brand
+function harmadikFeladat() {
+    let megoldas = [];
+    let a = elsoFeladat();
     let legolcsobbÁtlagÁr;
-    let sörÁtlagÁrak=[];
-    a.forEach((item)=>{
-        let átlagÁr=0;
+    let sörÁtlagÁrak = [];
+    let átlagÁr;
+    a.forEach((item) => {
+        átlagÁr = 0;
         for (let index = 0; index < item.beers.length; index++) {
-            átlagÁr = átlagÁr+parseInt(item.beers[index].price);         
-        }
-        átlagÁr= átlagÁr/item.beers.length;
+            átlagÁr = átlagÁr + parseFloat(item.beers[index].price);
+        };
+        átlagÁr = átlagÁr / item.beers.length;
         sörÁtlagÁrak.push(átlagÁr);
-        item['átlagár']=átlagÁr;
-    })
-    console.log(sörÁtlagÁrak)
-    //legolcsobb sörÁtlagár megkeresése
-    legolcsobbÁtlagÁr=Math.min.apply(null, sörÁtlagÁrak);
-    
-    //azzal az esettel is számoltam, ha két "legolcsóbb" brand is lenne
-    a.forEach((item)=>{
-        if (legolcsobbÁtlagÁr==item['átlagár']) {
-            megoldas.push(item)
-        }
-    })
-    
-    //Legolcsóbb brand nevek kigyűjtése
-    const megoldasNévVagyNevek=[];
-    megoldas.forEach((item)=>{
-        megoldasNévVagyNevek.push(item.brand)
+        item['átlagár'] = átlagÁr;
     });
 
+    //legolcsobb sörÁtlagár megkeresése
+    legolcsobbÁtlagÁr = Math.min.apply(null, sörÁtlagÁrak);
+
+    //azzal az esettel is számoltam, ha két "legolcsóbb" brand is lenne
+    a.forEach((item) => {
+        if (legolcsobbÁtlagÁr == item['átlagár']) {
+            megoldas.push(item)
+        }
+    });
+
+    //Legolcsóbb brand nevek kigyűjtése
+    const megoldasNévVagyNevek = [];
+    megoldas.forEach((item) => {
+        megoldasNévVagyNevek.push(item.brand)
+    });
     return megoldasNévVagyNevek;
-})();
+};
 
+// Which beers contain a specific ingredient
+function VasilyGrigoryevichZaytsev(ingredientInput) {
+    let actualRatio;
+    let megoldas = [];
+    if (ingredientInput == "cukor" || ingredientInput == "só" || ingredientInput == "árpa") {
+        for (let i = 0; i < beerList.length; i++) {
 
-(function otodikFeladat(){
-    let a =elsoFeladat();
-    let sumOfRatio=0;
+            for (let y = 0; y < beerList[i].ingredients.length; y++) {
+                actualRatio = parseFloat(beerList[i].ingredients[y].ratio);
+                if (actualRatio == 0 && ingredientInput == beerList[i].ingredients[y].name) {
+                    megoldas.push(beerList[i])
+                }
+            }
+        }
+    } else {
+        alert('Kérlek válassz a következő lehetőségek közül : "cukor" , "árpa", "só" ! A paramétert pontosan add meg ("" között), érdemes innen kimásolnod.');
+
+        return false;
+    }
+
+    return megoldas;
+};
+
+// List all beers with Water ingredient ratio (remaining ratio) - sort by ratio
+function otodikFeladat() {
+
+    let sumOfRatio = 0;
 
     for (let index = 0; index < beerList.length; index++) {
         for (let y = 0; y < beerList[index].ingredients.length; y++) {
-            let currentRatio= beerList[index].ingredients[y].ratio;
-            sumOfRatio=sumOfRatio+parseFloat(currentRatio);
-            
-        }
-        sumOfRatio= 1-sumOfRatio;
-        beerList[index]['sumOfRatio']=sumOfRatio;
-        //console.log(sumOfRatio)
-        sumOfRatio=0;
-        
+            let currentRatio = beerList[index].ingredients[y].ratio;
+            sumOfRatio = sumOfRatio + parseFloat(currentRatio);
+
+        };
+        sumOfRatio = 1 - sumOfRatio;
+        beerList[index]['sumOfRatio'] = sumOfRatio;
+        sumOfRatio = 0;
+
     }
     //az eredeti sorrend megtartása
     let megoldas = JSON.parse(JSON.stringify(beerList));
-    megoldas.sort(function(a, b) {
+    megoldas.sort(function (a, b) {
         return b.sumOfRatio - a.sumOfRatio;
-      });
-    //console.log(megoldas)
-
+    });
     return megoldas;
-})();
+};
 
-
-(function hatodikFeladat(){
+// Bonus - Hashmap based on properties
+function hatodikFeladat() {
     let megoldas = new Map();
-    
     //uniquePrices
     let prices = [];
-    beerList.forEach((item)=>{
-         if (!prices.includes(item.price)) {
-             prices.push(item.price)
-         }
+    beerList.forEach((item) => {
+        if (!prices.includes(item.price)) {
+            prices.push(item.price)
+        }
     });
     //sorba rendezés
-    prices.sort((a,b)=>{
-        return b-a;
+    prices.sort((a, b) => {
+        return b - a;
     });
 
-    let currentHoundred=0;
+    let currentHoundred = 0;
     let köztes = 0;
-    let justDoIt=prices.length;
-    let vlmi=[];
+    let justDoIt = prices.length;
+    let beszédesNév = [];
     do {
-        vlmi=[];
-        currentHoundred= Math.ceil(prices[justDoIt] / 100) * 100;
+        beszédesNév = [];
+        currentHoundred = Math.ceil(prices[justDoIt] / 100) * 100;
         beerList.forEach(element => {
-            if (element.price==prices[justDoIt]) {
-                vlmi.push(element)
+            if (element.price == prices[justDoIt]) {
+                beszédesNév.push(element)
             }
         });
         if (currentHoundred) {
             if (megoldas.has(currentHoundred)) {
                 //nem kell uj ertek
-                
-                köztes = megoldas.get(currentHoundred).push(vlmi)
+                köztes = megoldas.get(currentHoundred).push(beszédesNév)
             }
-            else{
-                megoldas.set(currentHoundred,vlmi)
-    
-    
+            else {
+                megoldas.set(currentHoundred, beszédesNév)
             }
         }
-        justDoIt=justDoIt-1;
-    } while (justDoIt);   
-      console.log(megoldas)
-
-})();
+        justDoIt = justDoIt - 1;
+    } while (justDoIt);
+    return megoldas;
+};
 
 
